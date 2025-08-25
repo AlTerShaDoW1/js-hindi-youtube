4fbaac17f23dcb5c0a3d8c16dbb75c3a0ff7f203
AlTerShaDoW1 <169084359+AlTerShaDoW1@users.noreply.github.com>
2025-08-25 23:25:22 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -6,7 +6,7 @@ const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
-const outsideTemp = null
+const outsideTemp = null (null means empty)
 let userEmail;
 
 const id = Symbol('123')
@@ -14,16 +14,16 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n (bigInt type)
 
 
 
-// Reference (Non primitive)
+// Reference (Non primitive) (Call by reference)
 
-// Array, Objects, Functions
+// Array, Objects, Functions (datatype of reference datatypes: Function)
 
 const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
+let myObj = {    //object
     name: "hitesh",
     age: 22,
 }
